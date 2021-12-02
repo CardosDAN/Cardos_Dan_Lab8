a18f42ba6b8f8d8340e091270bdcfb5b75428d9e
cardosdan <cardosdan08@gmail.com>
2021-12-02 15:14:40 +0200

    Lab 8

---
+++ Data/Cardos_Dan_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cardos_Dan_Lab8.Models;
+
+namespace Cardos_Dan_Lab8.Data
+{
+    public class Cardos_Dan_Lab8Context : DbContext
+    {
+        public Cardos_Dan_Lab8Context (DbContextOptions<Cardos_Dan_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cardos_Dan_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Cardos_Dan_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Cardos_Dan_Lab8.Models.Category> Category { get; set; }
+    }
+}
